f88e5882e5a2eef2e5cf339ff639fd480d7a5791
Eric Sondergard <4650644+esond@users.noreply.github.com>
2023-12-01 09:34:11 -0600

    Add overloads that use System.Uri to HTTP extensions

--- src/Common/Http/HttpClientExtensions.cs
+++ src/Common/Http/HttpClientExtensions.cs
@@ -1,36 +1,60 @@
 using System.Net.Http.Json;
 using System.Text;
 using System.Text.Json;
-using Hexagrams.Extensions.Common.Http;
 using Hexagrams.Extensions.Common.Serialization;
 
 namespace Hexagrams.Extensions.Common.Http;
 
 /// <summary>
-/// Extension methods for <see cref="HttpClient"/>.
+/// Extension methods for <see cref="HttpClient" />.
 /// </summary>
 public static class HttpClientExtensions
 {
     /// <summary>
-    /// Sends a POST request to the specified Uri containing the <paramref name="value"/> serialized as JSON in the
-    /// request body and deserializes the response from JSON to a new instance of <typeparamref name="TResponse"/>.
+    /// Sends a POST request to the specified Uri containing the <paramref name="value" /> serialized as JSON in the
+    /// request body and deserializes the response from JSON to a new instance of <typeparamref name="TResponse" />.
     /// </summary>
     /// <typeparam name="TRequest">The type of the value to serialize.</typeparam>
     /// <typeparam name="TResponse">The type of the response to deserialize.</typeparam>
     /// <param name="client">The client used to send the request.</param>
-    /// <param name="requestUri">The Uri the request is sent to.</param>
+    /// <param name="requestUri">The URI the request is sent to.</param>
     /// <param name="value">The value to serialize.</param>
     /// <param name="options">
     /// Options to control the behavior during serialization. The default options are those specified by
-    /// <see cref="JsonSerializerDefaults.Web"/>.
+    /// <see cref="JsonSerializerDefaults.Web" />.
     /// </param>
     /// <param name="cancellationToken">
     /// A cancellation token that can be used by other objects or threads to receive notice of cancellation.
     /// </param>
     /// <returns>The task object representing the asynchronous operation.</returns>
-    /// <exception cref="ArgumentNullException">The <paramref name="client"/> is null.</exception>
+    /// <exception cref="ArgumentNullException">The <paramref name="client" /> is null.</exception>
     /// <exception cref="HttpRequestException">The request was unsuccessful.</exception>
-    public static async Task<TResponse?> PostAsJsonAsync<TRequest, TResponse>(this HttpClient client, string? requestUri,
+    public static Task<TResponse?> PostAsJsonAsync<TRequest, TResponse>(this HttpClient client, string? requestUri,
+        TRequest value, JsonSerializerOptions? options = null, CancellationToken cancellationToken = default)
+    {
+        return client.PostAsJsonAsync<TRequest, TResponse>(CreateUri(requestUri), value, options, cancellationToken);
+    }
+
+    /// <summary>
+    /// Sends a POST request to the specified Uri containing the <paramref name="value" /> serialized as JSON in the
+    /// request body and deserializes the response from JSON to a new instance of <typeparamref name="TResponse" />.
+    /// </summary>
+    /// <typeparam name="TRequest">The type of the value to serialize.</typeparam>
+    /// <typeparam name="TResponse">The type of the response to deserialize.</typeparam>
+    /// <param name="client">The client used to send the request.</param>
+    /// <param name="requestUri">The <see cref="Uri" /> the request is sent to.</param>
+    /// <param name="value">The value to serialize.</param>
+    /// <param name="options">
+    /// Options to control the behavior during serialization. The default options are those specified by
+    /// <see cref="JsonSerializerDefaults.Web" />.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// A cancellation token that can be used by other objects or threads to receive notice of cancellation.
+    /// </param>
+    /// <returns>The task object representing the asynchronous operation.</returns>
+    /// <exception cref="ArgumentNullException">The <paramref name="client" /> is null.</exception>
+    /// <exception cref="HttpRequestException">The request was unsuccessful.</exception>
+    public static async Task<TResponse?> PostAsJsonAsync<TRequest, TResponse>(this HttpClient client, Uri? requestUri,
         TRequest value, JsonSerializerOptions? options = null, CancellationToken cancellationToken = default)
     {
         var response = await client.PostAsJsonAsync(requestUri, value, options, cancellationToken)
@@ -43,25 +67,50 @@ public static class HttpClientExtensions
     }
 
     /// <summary>
-    /// Sends a PUT request to the specified Uri containing the <paramref name="value"/> serialized as JSON in the
-    /// request body and deserializes the response from JSON to a new instance of <typeparamref name="TResponse"/>.
+    /// Sends a PUT request to the specified Uri containing the <paramref name="value" /> serialized as JSON in the
+    /// request body and deserializes the response from JSON to a new instance of <typeparamref name="TResponse" />.
     /// </summary>
     /// <typeparam name="TRequest">The type of the value to serialize.</typeparam>
     /// <typeparam name="TResponse">The type of the response to deserialize.</typeparam>
     /// <param name="client">The client used to send the request.</param>
-    /// <param name="requestUri">The Uri the request is sent to.</param>
+    /// <param name="requestUri">The URI the request is sent to.</param>
     /// <param name="value">The value to serialize.</param>
     /// <param name="options">
     /// Options to control the behavior during serialization. The default options are those specified by
-    /// <see cref="JsonSerializerDefaults.Web"/>.
+    /// <see cref="JsonSerializerDefaults.Web" />.
     /// </param>
     /// <param name="cancellationToken">
     /// A cancellation token that can be used by other objects or threads to receive notice of cancellation.
     /// </param>
     /// <returns>The task object representing the asynchronous operation.</returns>
-    /// <exception cref="ArgumentNullException">The <paramref name="client"/> is null.</exception>
+    /// <exception cref="ArgumentNullException">The <paramref name="client" /> is null.</exception>
     /// <exception cref="HttpRequestException">The request was unsuccessful.</exception>
-    public static async Task<TResponse?> PutAsJsonAsync<TRequest, TResponse>(this HttpClient client, string? requestUri,
+    public static Task<TResponse?> PutAsJsonAsync<TRequest, TResponse>(this HttpClient client, string? requestUri,
+        TRequest value, JsonSerializerOptions? options = null, CancellationToken cancellationToken = default)
+    {
+        return client.PutAsJsonAsync<TRequest, TResponse>(CreateUri(requestUri), value, options, cancellationToken);
+    }
+
+    /// <summary>
+    /// Sends a PUT request to the specified Uri containing the <paramref name="value" /> serialized as JSON in the
+    /// request body and deserializes the response from JSON to a new instance of <typeparamref name="TResponse" />.
+    /// </summary>
+    /// <typeparam name="TRequest">The type of the value to serialize.</typeparam>
+    /// <typeparam name="TResponse">The type of the response to deserialize.</typeparam>
+    /// <param name="client">The client used to send the request.</param>
+    /// <param name="requestUri">The <see cref="Uri" /> the request is sent to.</param>
+    /// <param name="value">The value to serialize.</param>
+    /// <param name="options">
+    /// Options to control the behavior during serialization. The default options are those specified by
+    /// <see cref="JsonSerializerDefaults.Web" />.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// A cancellation token that can be used by other objects or threads to receive notice of cancellation.
+    /// </param>
+    /// <returns>The task object representing the asynchronous operation.</returns>
+    /// <exception cref="ArgumentNullException">The <paramref name="client" /> is null.</exception>
+    /// <exception cref="HttpRequestException">The request was unsuccessful.</exception>
+    public static async Task<TResponse?> PutAsJsonAsync<TRequest, TResponse>(this HttpClient client, Uri? requestUri,
         TRequest value, JsonSerializerOptions? options = null, CancellationToken cancellationToken = default)
     {
         var response = await client.PutAsJsonAsync(requestUri, value, options, cancellationToken).ConfigureAwait(false);
@@ -73,40 +122,73 @@ public static class HttpClientExtensions
     }
 
     /// <summary>
-    /// Sends a DELETE request to the specified Uri containing the <paramref name="value"/> serialized as JSON in the
+    /// Sends a DELETE request to the specified Uri containing the <paramref name="value" /> serialized as JSON in the
     /// request body.
     /// </summary>
     /// <typeparam name="TRequest">The type of the value to serialize.</typeparam>
     /// <param name="client">The client used to send the request.</param>
-    /// <param name="requestUri">The Uri the request is sent to.</param>
+    /// <param name="requestUri">The URI the request is sent to.</param>
     /// <param name="value">The value to serialize.</param>
     /// <param name="options">
     /// Options to control the behavior during serialization. The default options are those specified by
-    /// <see cref="JsonSerializerDefaults.Web"/>.
+    /// <see cref="JsonSerializerDefaults.Web" />.
     /// </param>
     /// <param name="cancellationToken">
     /// A cancellation token that can be used by other objects or threads to receive notice of cancellation.
     /// </param>
     /// <returns>The task object representing the asynchronous operation.</returns>
-    /// <exception cref="ArgumentNullException">The <paramref name="client"/> is null.</exception>
-    /// <exception cref="ArgumentNullException">The <paramref name="value"/> is null.</exception>
+    /// <exception cref="ArgumentNullException">The <paramref name="client" /> is null.</exception>
+    /// <exception cref="ArgumentNullException">The <paramref name="value" /> is null.</exception>
+    /// <exception cref="HttpRequestException">The request was unsuccessful.</exception>
+    public static Task<HttpResponseMessage?> DeleteAsJsonAsync<TRequest>(this HttpClient client, string? requestUri,
+        TRequest value, JsonSerializerOptions? options = null, CancellationToken cancellationToken = default)
+    {
+        return client.DeleteAsJsonAsync(CreateUri(requestUri), value, options, cancellationToken);
+    }
+
+    /// <summary>
+    /// Sends a DELETE request to the specified Uri containing the <paramref name="value" /> serialized as JSON in the
+    /// request body.
+    /// </summary>
+    /// <typeparam name="TRequest">The type of the value to serialize.</typeparam>
+    /// <param name="client">The client used to send the request.</param>
+    /// <param name="requestUri">The <see cref="Uri" /> the request is sent to.</param>
+    /// <param name="value">The value to serialize.</param>
+    /// <param name="options">
+    /// Options to control the behavior during serialization. The default options are those specified by
+    /// <see cref="JsonSerializerDefaults.Web" />.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// A cancellation token that can be used by other objects or threads to receive notice of cancellation.
+    /// </param>
+    /// <returns>The task object representing the asynchronous operation.</returns>
+    /// <exception cref="ArgumentNullException">The <paramref name="client" /> is null.</exception>
+    /// <exception cref="ArgumentNullException">The <paramref name="value" /> is null.</exception>
     /// <exception cref="HttpRequestException">The request was unsuccessful.</exception>
     public static async Task<HttpResponseMessage?> DeleteAsJsonAsync<TRequest>(this HttpClient client,
-        string? requestUri, TRequest value, JsonSerializerOptions? options = null,
+        Uri? requestUri, TRequest value, JsonSerializerOptions? options = null,
         CancellationToken cancellationToken = default)
     {
-        if (value is null)
-            throw new ArgumentNullException(nameof(value));
+        ArgumentNullException.ThrowIfNull(client);
+        ArgumentNullException.ThrowIfNull(value);
 
         options ??= new JsonSerializerOptions(JsonSerializerDefaults.Web);
 
-        var request = new HttpRequestMessage(HttpMethod.Delete, requestUri)
-        {
-            Content = new StringContent(value.ToJson(options), Encoding.UTF8, "application/json")
-        };
+        using var request = new HttpRequestMessage();
+
+        request.Method = HttpMethod.Delete;
+        request.RequestUri = requestUri;
+        request.Content = new StringContent(value.ToJson(options), Encoding.UTF8, "application/json");
 
         var response = await client.SendAsync(request, cancellationToken).ConfigureAwait(false);
 
+        response.RequestMessage = request;
+
         return response.EnsureSuccessStatusCode();
+    }
+
+    private static Uri? CreateUri(string? uri)
+    {
+        return string.IsNullOrEmpty(uri) ? null : new Uri(uri, UriKind.RelativeOrAbsolute);
     }
 }
